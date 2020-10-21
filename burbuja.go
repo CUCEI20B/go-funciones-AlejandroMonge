9587e876b761aef871e831d79d63648c3a8de3ed
AlejandroMonge <kmonge_rey@hotmail.com>
2020-10-21 12:59:03 -0500

    Prueba01

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,19 @@
 package main
 
-func Burbuja(s []int64)  {
+func Burbuja(s []int64) {
 
+	var auxiliar int64
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s); j++ {
+			if s[i] < s[j] {
+				auxiliar = s[i]
+				s[i] = s[j]
+				s[j] = auxiliar
+			}
+		}
+	}
 }
 
-func main()  {
-	
+func main() {
+
 }
